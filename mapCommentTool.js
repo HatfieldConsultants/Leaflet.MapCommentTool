34901832dd1077f99283a1ccf53b59df833b3b26
Nima Boscarino <nboscarino@hatfieldgroup.com>
2016-11-08 15:11:35 -0800

    Tests for checking toggling of control panel

--- mapCommentTool.js
+++ mapCommentTool.js
@@ -14,11 +14,112 @@
         window.L.MapCommentTool = factory(L);
     }
 }(function (L) {
-    var MapCommentTool = {};
-    // implement your plugin
-    MapCommentTool.sayHello = function() {
-    	return 'hello world';
+    var MapCommentTool = {
+    };
+
+    MapCommentTool.options = {
+        position: 'right',
     }
+
+    // implement your plugin
+    MapCommentTool.getMessage = function() {
+    	return 'Map Comment Tool';
+    }
+
+    MapCommentTool.addTo = function(map) {
+        var self = this;
+
+        var visibileClass = (self.ControlBar.isVisible()) ? 'visible' : '';
+
+        // Create sidebar container
+        var container = self.ControlBar._container =
+            L.DomUtil.create('div', 'leaflet-control-bar-'+self.options.position+' leaflet-control-bar ' + visibileClass);
+        var content = self.ControlBar._contentContainer;
+
+        L.DomEvent
+            .on(container, 'transitionend',
+            self.ControlBar._handleTransitionEvent, self)
+            .on(container, 'webkitTransitionEnd',
+            self.ControlBar._handleTransitionEvent, self);
+
+        var controlContainer = map._controlContainer;
+        controlContainer.insertBefore(container, controlContainer.firstChild);
+
+        self._map = map;
+
+        map.MapCommentTool = MapCommentTool;
+
+
+    }
+
+    MapCommentTool.ControlBar = {
+        visible: false,
+        isVisible: function() {
+            var self = this;
+            return self.visible;
+        },
+        show: function() {
+            var self = this;
+            self.visible = true;
+
+            L.DomUtil.addClass(self._container, 'visible');
+
+            var controls = document.getElementsByClassName("leaflet-control leaflet-bar");
+            for (var i = 0; i < controls.length; i++) {
+                controls[i].style.visibility='hidden';
+            }
+
+            map.dragging.disable();
+            map.touchZoom.disable();
+            map.doubleClickZoom.disable();
+            map.scrollWheelZoom.disable();
+            map.boxZoom.disable();
+            map.keyboard.disable();
+            if (map.tap) map.tap.disable();
+            document.getElementById('map').style.cursor='default';
+            
+            // on success, should return true
+            return true;
+        },
+        hide: function(e) {
+            var self = this;
+            self.visible = false;
+            
+            L.DomUtil.removeClass(self._container, 'visible');
+            var controls = document.getElementsByClassName("leaflet-control leaflet-bar");
+            for (var i = 0; i < controls.length; i++) {
+                controls[i].style.visibility='visible';
+            }
+            if(e) {
+                L.DomEvent.stopPropagation(e);
+            }
+            map.dragging.enable();
+            map.touchZoom.enable();
+            map.doubleClickZoom.enable();
+            map.scrollWheelZoom.enable();
+            map.boxZoom.enable();
+            map.keyboard.enable();
+            if (map.tap) map.tap.enable();
+            document.getElementById('map').style.cursor='grab';
+            // on success, should return true
+            return true;
+        },
+        toggle: function() {
+            var self = this;
+            
+            var toggleSuccess = self.isVisible() ? self.hide() : self.show();
+
+            return toggleSuccess;
+        },
+
+        _handleTransitionEvent: function (e) {
+            var self = this;
+            //if (e.propertyName == 'left' || e.propertyName == 'right' ||e.propertyName == 'bottom' || e.propertyName == 'top')
+                //self.fire(self.ControlBar.isVisible() ? 'shown' : 'hidden');
+        }
+
+    }
+
     // return your plugin when you are done
     return MapCommentTool;
 }, window));
--- test/test.js
+++ test/test.js
@@ -5,8 +5,49 @@ describe('Map Loaded Properly', function() {
 	it('getCenter().lat should return proper lat', function() {
 		assert.equal(map.getCenter().lat, 49.2827 );
 	});
-
 	it('getCenter().lng should return proper lng', function() {
 		assert.equal(map.getCenter().lng, -123.1207 );
 	});
+});
+
+describe('Plugin Loaded Properly', function() {
+
+	it('getMessage() should return "Map Comment Tool"', function() {
+		assert.equal(map.MapCommentTool.getMessage(), 'Map Comment Tool' );
+	});
+	
+});
+
+describe('Initial Control Bar Status', function() {
+
+	it('isVisible() should be "false"', function() {
+		assert.isNotOk(window.L.MapCommentTool.ControlBar.isVisible());
+	});
+
+});
+
+describe('Control Bar Show/Hide', function() {
+
+	it('isVisible() should initially be "false"', function() {
+		assert.isNotOk(window.L.MapCommentTool.ControlBar.isVisible());
+	});
+
+	it('show() should return "true" and change "visible" to true', function() {
+		assert.isOk(window.L.MapCommentTool.ControlBar.show(), 'show() executed successfully');
+		assert.isOk(window.L.MapCommentTool.ControlBar.isVisible());
+	});
+	
+	it('hide() should return "true" and change "visible" to false', function() {
+		assert.isOk(window.L.MapCommentTool.ControlBar.hide(), 'hide() executed successfully');
+		assert.isNotOk(window.L.MapCommentTool.ControlBar.isVisible());
+	});
+
+	it('if not visible, 2 toggles should trigger show(), and then hide()', function() {
+		assert.isNotOk(window.L.MapCommentTool.ControlBar.isVisible(), 'initial visibility state is hidden');
+		assert.isOk(window.L.MapCommentTool.ControlBar.toggle(), 'toggle() executed successfully');
+		assert.isOk(window.L.MapCommentTool.ControlBar.isVisible(), 'visibility state now hidden');
+		assert.isOk(window.L.MapCommentTool.ControlBar.toggle(), 'toggle() executed successfully');
+		assert.isNotOk(window.L.MapCommentTool.ControlBar.isVisible(), 'visibility state hidden once again');
+	});
+
 });
